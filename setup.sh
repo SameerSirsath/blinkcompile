4528a880c98e7f8861360491e254c77b241d71e7
Sameer Sirsath <sameersirsath2503@outlook.com>
2026-01-27 13:40:32 +0530

    Add setup steps

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip install --upgrade pip
+pip install tensorflow-cpu==2.10.0 --no-deps
+pip install streamlit==1.28.0 Pillow==10.0.0 qrcode==7.4.2 psutil==5.9.5
+pip install transformers==4.30.0
+pip install numpy==1.23.5 onnx==1.13.1 torch==1.13.1
